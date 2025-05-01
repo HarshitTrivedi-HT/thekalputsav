d02d4f5e3bf1ea47927e276a92f4278f0fc5afd8
HarshitTrivedi-HT <101573067+HarshitTrivedi-HT@users.noreply.github.com>
2025-05-01 15:47:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// // function f1() {
+// //   window.open("file:///C:/Users/trive/OneDrive/Desktop/WebDev/hi.html");
+// }
+// function sc() {
+//   window.open("file:///C:/Users/trive/OneDrive/Desktop/WebDev/sc.html");
+// }
+// function ma() {
+//   window.open("file:///C:/Users/trive/OneDrive/Desktop/WebDev/ma.html");
+// }
+// function sst() {
+//   window.open("file:///C:/Users/trive/OneDrive/Desktop/WebDev/sst.html");
+// }
