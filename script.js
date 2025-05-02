ef802d30f76d3b56508be2316b08fb075f4ab560
HarshitTrivedi-HT <101573067+HarshitTrivedi-HT@users.noreply.github.com>
2025-05-02 16:06:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-// // function f1() {
-// //   window.open("file:///C:/Users/trive/OneDrive/Desktop/WebDev/hi.html");
-// }
-// function sc() {
-//   window.open("file:///C:/Users/trive/OneDrive/Desktop/WebDev/sc.html");
-// }
-// function ma() {
-//   window.open("file:///C:/Users/trive/OneDrive/Desktop/WebDev/ma.html");
-// }
-// function sst() {
-//   window.open("file:///C:/Users/trive/OneDrive/Desktop/WebDev/sst.html");
-// }
+function submitfeed(){
+    alert("Thanks for your feedback");
+    const feed = document.getElementById('feed');
+    feed.value = '';
+
+
+}
